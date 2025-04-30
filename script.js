f2943866db7925db68c4626da4d6d263ab09483d
Marzi Ghasedian <marzighased@gmail.com>
2025-04-30 18:26:34 +0200

    guess command added

--- script.js
+++ script.js
@@ -6,4 +6,11 @@ const message = document.getElementById('message');
 
 guessBtn.addEventListener('click', function() {
     const userGuess = Number(guessInput.value);
+
+    if (!userGuess || userGuess < 1 || userGuess > 100) {
+        message.textContent = 'Enter a Number between 1-100';
+    } else if (userGuess === secretNumber) {
+        message.textContent = 'Correct!';
+        message.style.color = 'green';
+    } 
 })
